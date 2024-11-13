4ed83e6c623362b7122da0dce3ea8ebac69237e2
Muhammad Ali <malimaken772@gmail.com>
2024-11-13 11:10:34 +0500

    Implemented getUrlsFromHtml and all tests passed

--- web-crawlers/crawl.test.ts
+++ web-crawlers/crawl.test.ts
@@ -1,5 +1,5 @@
 // crawl.test.ts
-import { normalizeUrl } from "./crawl";
+import { getUrlsFromHtml, normalizeUrl } from "./crawl";
 import { test, expect } from "@jest/globals";
 
 /*
@@ -13,6 +13,7 @@ test("normalize strip protocal", () => {
   const input = "https://boot.dev/path";
   const output = normalizeUrl(input);
   const expected = "boot.dev/path";
+
   expect(output).toEqual(expected);
 });
 
@@ -20,6 +21,7 @@ test("normalize strip trailing slashes", () => {
   const input = "https://boot.dev/path/";
   const output = normalizeUrl(input);
   const expected = "boot.dev/path";
+
   expect(output).toEqual(expected);
 });
 
@@ -27,6 +29,7 @@ test("normalize strip capitals", () => {
   const input = "https://BOOT.dev/path/";
   const output = normalizeUrl(input);
   const expected = "boot.dev/path";
+
   expect(output).toEqual(expected);
 });
 
@@ -34,5 +37,129 @@ test("normalize strip http", () => {
   const input = "http://BOOT.dev/path/";
   const output = normalizeUrl(input);
   const expected = "boot.dev/path";
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml absolute path", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="https://blog.boot.dev/path">
+        Boot.dev Example Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev/path";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected = ["https://blog.boot.dev/path"];
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml relative path", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="/path">
+        Boot.dev Example Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected = ["https://blog.boot.dev/path"];
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml multiple paths absolute", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="https://blog.boot.dev/path1">
+        Boot.dev Example One Html Body
+      </a>
+
+      <a href="https://blog.boot.dev/path2">
+        Boot.dev Example Two Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected = [
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2",
+  ];
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml multiple paths relative", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="/path1">
+        Boot.dev Example One Html Body
+      </a>
+
+      <a href="/path2">
+        Boot.dev Example Two Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected = [
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2",
+  ];
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml multiple paths absolute relative", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="https://blog.boot.dev/path1">
+        Boot.dev Example One Html Body
+      </a>
+
+      <a href="/path2">
+        Boot.dev Example Two Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected = [
+    "https://blog.boot.dev/path1",
+    "https://blog.boot.dev/path2",
+  ];
+
+  expect(output).toEqual(expected);
+});
+
+test("getUrlsFromHtml invalid url", () => {
+  const inputHtml = `
+  <html>
+    <body>
+      <a href="invalid path">
+        Boot.dev Example Two Html Body
+      </a>
+    </body>
+  </html>
+  `;
+  const inputUrl = "https://blog.boot.dev";
+  const output = getUrlsFromHtml(inputHtml, inputUrl);
+  const expected: any = [];
+
   expect(output).toEqual(expected);
 });
--- web-crawlers/crawl.ts
+++ web-crawlers/crawl.ts
@@ -1,4 +1,34 @@
-// crawl.ts
+import { JSDOM } from "jsdom";
+
+export function getUrlsFromHtml(htmlBody: string, urlPath: string): any[] {
+  const urls: string[] = [];
+  const domObj = new JSDOM(htmlBody);
+
+  const linkElements = domObj.window.document.querySelectorAll("a");
+
+  Array.from(linkElements).forEach((link) => {
+    if (link.href.slice(0, 1) == "/") {
+      /* RELATIVE */
+      try {
+        const urlObj = new URL(`${urlPath}${link.href}`);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("Error : ", err);
+      }
+    } else {
+      /* ABSOLUTE */
+      try {
+        const urlObj = new URL(link.href);
+        urls.push(urlObj.href);
+      } catch (err) {
+        console.log("Error : ", err);
+      }
+    }
+  });
+
+  return urls;
+}
+
 export function normalizeUrl(urlString: string): string {
   const urlObj = new URL(urlString);
 
